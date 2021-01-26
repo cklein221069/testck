b4fe8e947e6b3579edd21a7719515a041b1143df
klein <klein_c@hotmail.fr>
2021-01-26 10:23:33 +0100

    testwq

---
+++ src/main/java/cog/exemple/demo/App.java
@@ -0,0 +1,13 @@
+package cog.exemple.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
